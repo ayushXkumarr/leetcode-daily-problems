09de56047d11bb74390fff5d1764b7f29c76dd4a
Ayush Kumar <152773842+ayushXkumarr@users.noreply.github.com>
2026-07-16 22:37:41 +0530

    Time: 3 ms (92.59%) | Memory: 44.5 MB (81.60%) - LeetSync

--- 1044-find-common-characters/find-common-characters.java
+++ 1044-find-common-characters/find-common-characters.java
@@ -1,32 +1,30 @@
 class Solution {
     public List<String> commonChars(String[] words) {
-        HashMap <Character,Integer> map = new HashMap<>();
-        for(char ch:words[0].toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
+        int [] freq = new int[26];
+        for(char ch: words[0].toCharArray()){
+            freq[ch - 'a']++;
+        } 
 
         for(int i=1;i<words.length;i++){
-           HashMap <Character,Integer> curr = new HashMap<>(); 
+            int [] curr = new int[26];
 
-           for(char ch:words[i].toCharArray()){
-            curr.put(ch,curr.getOrDefault(ch,0)+1);
+            for(char ch: words[i].toCharArray()){
+            curr[ch - 'a']++;
             }
 
-            for(char ch:map.keySet()){
-                int past = map.get(ch);
-                int New= curr.getOrDefault(ch,0);
-
-                if(past > New){
-                    map.put(ch,New);
+            for(int j=0;j<26;j++){
+                if(freq[j] > curr[j]){
+                    freq[j] = curr[j];
                 }
             }
         }
+
         List<String> ans = new ArrayList<>();
-        for(Map.Entry<Character,Integer> e:map.entrySet()){
-            int val = e.getValue();
-            while (val > 0) {
-                ans.add(String.valueOf(e.getKey()));
-                val--;
+
+        for(int i=0;i<26;i++){
+            while(freq[i]>0){
+                ans.add(String.valueOf((char) (i +'a')));
+                freq[i]--;
             }
         }
 
